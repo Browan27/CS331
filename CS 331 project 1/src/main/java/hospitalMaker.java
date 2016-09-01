714d38187617f19ee8c86e4e5991f2cb7672575d
HyderickCSarrell <hyderickc@live.com>
2016-09-01 18:00:54 -0500

    ITS OVER

--- CS 331 project 1/src/main/java/hospitalMaker.java
+++ CS 331 project 1/src/main/java/hospitalMaker.java
@@ -1,25 +1,33 @@
 
-import java.io.PrintWriter;
-import java.io.File;
+import java.io.*;
 import java.util.Random;
 
 public class hospitalMaker {
     
     private static String fileName;
+	
+	
 
-    public void FileCreator(String fileName) throws Exception{
+    public static void fileCreator(String s) throws FileNotFoundException {
 		
-		PrintWriter newFile = new PrintWriter(fileName);
+		PrintWriter newFile = new PrintWriter(s);
 		Random ranNumGen = new Random();
 			
 		for(int i = 0; i < 4; i++) {
 			for (int j = 0; j < 7; j++ ) {
 				for(int k = 0; k < 24; k++) {
-						newFile.println(ranNumGen.nextInt(10));
+					newFile.println(ranNumGen.nextInt(10));
 				}
 			}
 		}
 		newFile.close();
+	}
+	
+	public static void main(String[] args) {
+		fileName = "hospital.txt";
+		try {
+			fileCreator(fileName);
+		} catch(FileNotFoundException e) {System.out.println("wellcrap");}
 	}
 	
 }
