5622a0ff264005c2a081556a8aeb7431aa1eba8b
Browan27 <Browan27@yahoo.com>
2016-09-06 11:28:02 -0500

    major changes and foundations started

--- CS 331 project 1/src/main/java/hospital.java
+++ CS 331 project 1/src/main/java/hospital.java
@@ -8,25 +8,50 @@ public class hospital {
     private static String daysOfWeek[] = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     
     public hospital() {
-        hourCount = 1;
+        hourCount = 0;
         dayCount = 0;
         weekCount = 0;
+        storage = new int[4][7][24];
+    }
+    
+    public void fileReader() {
         
     }
     
-    public int fileReader() {
-        
-        return 0;
+    public void addToStorage(int x) {
+        storage[weekCount][dayCount][hourCount] = x;
+        hourCount++;
+        if(hourCount == 24) { hourCount = 0; dayCount++; }
+        if(dayCount == 7) { dayCount = 0; weekCount++; }
     }
     
-    public void arraySet(int x, int y, int z, int a) {
-        
+    public int calculateAverageBasic(int[] array) {
+        int average = 0;
+        for(int i = 0; i < array.length; i++) {
+            average += array[i];
+        }
+        average = average/array.length;
+        return average;
     }
     
-    public int calculateAverage() {
-        
-        return 0;
+    public int returnStorageLast() {
+        if(hourCount == 0) {
+            if(dayCount == 0) {
+                if(weekCount == 0) { return storage[0][0][0]; }
+                else { return storage[weekCount-1][6][23]; }
+            }
+            else {
+                return storage[weekCount][dayCount-1][23];
+            }
+        }
+        else { return storage[weekCount][dayCount][hourCount-1]; }
     }
+    public int returnStorageSpecific(int week, int day, int hour) {
+        return storage[week][day][hour];
+    }
+    public int returnHourCount() { return hourCount; }
+    public int returnDayCount() { return dayCount; }
+    public int returnWeekCount() { return weekCount; }
     
     public static void main(String[] args) {
         
--- CS 331 project 1/src/test/java/hospitalTest.java
+++ CS 331 project 1/src/test/java/hospitalTest.java
@@ -3,14 +3,41 @@ import static org.junit.Assert.*;
 import java.util.*;
 
 public class hospitalTest {
+    private hospital h = new hospital();
     
     @Test
-    public void testTrue() {
-        assertTrue(true);
+    public void calculateAverageTest() {
+    int[] array = {1, 2, 3};
+        assertEquals(2, h.calculateAverageBasic(array));
     }
     
     @Test
-    public void testFalse() {
-        assertTrue(false);
+    public void calculateAverageDeepTest() {
+    int[] array = {1, 7, 0, 9, 4, 2, 2, 4, 6, 3, 6, 9};
+        assertEquals(4, h.calculateAverageBasic(array));
+    }
+    
+    @Test
+    public void returnCountTest() {
+        assertEquals(0, h.returnHourCount());
+        assertEquals(0, h.returnDayCount());
+        assertEquals(0, h.returnWeekCount());
+    }
+    
+    @Test
+    public void returnCountAfterAddAndTheAddTest() {
+        assertEquals(0, h.returnHourCount());
+        h.addToStorage(4);
+        assertEquals(1, h.returnHourCount());
+        assertEquals(4, h.returnStorageLast());
+    }
+    
+    @Test
+    public void calculateArrayAverage() {
+        for(int i = 0; i < 24; i++) {
+            h.addToStorage(i+1);
+        }
+        assertEquals(1, h.returnDayCount());
+        assertEquals(0, h.returnHourCount());
     }
 }
